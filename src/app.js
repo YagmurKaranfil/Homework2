a740cb8dfd289644d14d07aa4b2310e40664599c
YagmurKaranfil <59535304+YagmurKaranfil@users.noreply.github.com>
2021-10-30 18:56:38 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,31 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarBugun =9.30
+let dolarDun =9.20
+dolarDun="9.20"
+
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+
+const euroDun =11.2
+// euroDun=11
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut kredisi ","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"]
+
+console.log("</ul")
+
+for(let i =0; i < konutKredileri.length; i++){
+   console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log("</ul")
+
+console.log(konutKredileri)
